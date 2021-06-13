bb39aa5c4aaafa12f24a6a4f8d14a22310ccab7b
root <root@mir.01.devnet>
2021-06-13 12:19:43 -0400

    first commit

---
+++ net_inv.py
@@ -0,0 +1,10 @@
+#! /home/administrator/code/venvs/devnetl1/devnetl1/bin/python3
+"""
+Basic Network Inventory Generation Script
+
+"""
+
+# Script Entry Point
+if __name__=="__main__"
+   print("Creating a network inventory script.")
+
